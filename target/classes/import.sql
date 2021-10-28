defc01e2b51a348b05b06a32c7c70f97776c63b6
gmendozac <ga.demendoza@mensajerosurbanos.com>
2021-10-28 12:11:37 -0500

    upload target

---
+++ target/classes/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO position (name) VALUES ('DEVELOPER');
+INSERT INTO position (name) VALUES ('TESTER');
+INSERT INTO position (name) VALUES ('MANAGER');
+INSERT INTO position (name) VALUES ('INFRAESTRUCTURE');
+INSERT INTO position (name) VALUES ('DEVOPS');
+INSERT INTO position (name) VALUES ('REQUIREMENTS');
+
